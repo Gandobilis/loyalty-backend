0762f734e5d9a777cb1894079944b2292446d12c
Gandobilis <Lashadeveloper@gmail.com>
2025-09-30 20:22:26 +0400

    Initial commit

---
+++ src/test/java/com/multi/loyaltybackend/LoyaltyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.loyaltybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
