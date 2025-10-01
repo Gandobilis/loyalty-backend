72931ff56e02bf3c71c1b9baeb849d7ba4f32ff2
Gandobilis <Lashadeveloper@gmail.com>
2025-10-01 20:48:58 +0400

    OpenApiConfig added.

--- src/main/java/com/multi/loyaltybackend/LoyaltyBackendApplication.java
+++ src/main/java/com/multi/loyaltybackend/LoyaltyBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoyaltyBackendApplication {
-
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(LoyaltyBackendApplication.class, args);
     }
-
 }
