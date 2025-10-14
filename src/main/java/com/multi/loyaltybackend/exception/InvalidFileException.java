3a86b8c679997031a15808e48c94d1436381ef5e
Gandobilis <Lashadeveloper@gmail.com>
2025-10-14 14:14:07 +0400

    profile final version.

--- src/main/java/com/multi/loyaltybackend/exception/InvalidFileException.java
+++
@@ -1,7 +0,0 @@
-package com.multi.loyaltybackend.exception;
-
-public class InvalidFileException extends RuntimeException {
-    public InvalidFileException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/multi/loyaltybackend/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.multi.loyaltybackend.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
